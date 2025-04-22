1b72de1cc3b4c22c9c505e09f51817b6074bab64
vipyan <vipinkaniyanthara@gmail.com>
2025-04-21 20:53:48 -0400

    Updated the logic to manipulate the DOM

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ form.addEventListener('submit',function(e){
         .then(data => {
         console.log(data.colors);
         console.log(data.colors[0].name.value)
+            console.log(data.colors[0].hex.value)
+            document.querySelector('.c1').style.backgroundColor = data.colors[0].hex.value
+            document.querySelector('.s1').textContent = data.colors[0].hex.value
+
+            document.querySelector('.c2').style.backgroundColor = data.colors[1].hex.value
+            document.querySelector('.s2').textContent = data.colors[1].hex.value
+
+            document.querySelector('.c3').style.backgroundColor = data.colors[2].hex.value
+            document.querySelector('.s3').textContent = data.colors[2].hex.value
+
+            document.querySelector('.c4').style.backgroundColor = data.colors[3].hex.value
+            document.querySelector('.s4').textContent = data.colors[3].hex.value
+
+            document.querySelector('.c5').style.backgroundColor = data.colors[4].hex.value
+            document.querySelector('.s5').textContent = data.colors[4].hex.value
+
         
         })
 })
